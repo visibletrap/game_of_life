5e36990809d592ae46d7f9a9f7f9549d278af5c1
Tap <visibletrap@gmail.com>
2013-05-11 14:30:30 +0800

    Move methods from gol to world

--- world.rb
+++ world.rb
@@ -14,4 +14,20 @@ class World
   def ==(other_world)
     self.cells == other_world.cells
   end
+
+  def next_gen
+    World.new(*(judge_living_cells + revive_dead_cells))
+  end
+
+  private
+  def judge_living_cells
+    cells.select { |c| [2,3].include?(c.alive_neighbours_count(cells)) }
+  end
+
+  def revive_dead_cells
+    cells.inject([]) { |a,c| a.concat c.neighbours.to_a }
+          .group_by { |i| i }
+          .select { |_,neighbours| neighbours.count == 3 }
+          .map { |(c,_)| c }.flatten
+  end
 end
--- world_spec.rb
+++ world_spec.rb
@@ -5,14 +5,44 @@ describe World do
 
   describe '==' do
     specify "world with the same cells" do
-      (World.new(Cell.new(0,0), Cell.new(0,1)) == World.new(Cell.new(0,0), Cell.new(0,1))).should be_true
+      (World.new(cell(0,0), cell(0,1)) == World.new(cell(0,0), cell(0,1))).should be_true
     end
   end
 
   describe 'include?' do
     it "checks whether it has cell" do
-      World.new(Cell.new(0,0)).include?(Cell.new(0,0)).should be_true
-      World.new.include?(Cell.new(0,0)).should be_false
+      World.new(cell(0,0)).include?(cell(0,0)).should be_true
+      World.new.include?(cell(0,0)).should be_false
     end
+  end
+
+  describe 'next_gen' do
+    describe 'a live cell' do
+      it 'dies with 0 live neighbour' do
+        World.new(cell(0,0)).next_gen.should == World.new
+      end
+
+      it 'stay alive with 2 live neighbours' do
+        World.new(cell(0,-1),cell(0,0),cell(0,1)).next_gen.should include(cell(0,0))
+      end
+
+      it 'stays alive with 3 live neighbours' do
+        World.new(cell(-1,-1),cell(-1,1),cell(0,0),cell(1,1)).next_gen.should include(cell(0,0))
+      end
+
+      it 'dies with 4 live neighbours' do
+        World.new(cell(-1,-1), cell(-1,1), cell(0,0), cell(1,-1), cell(1,1)).next_gen.should_not include(cell(0,0))
+      end
+    end
+
+    describe 'a dead cell' do
+      it "becomes alive with 3 neighbours" do
+        World.new(cell(-1,-1), cell(-1,1), cell(1,1)).next_gen.should include(cell(0,0))
+      end
+    end
+  end
+
+  def cell(x,y)
+    Cell.new(x,y)
   end
 end
