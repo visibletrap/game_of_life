58c851675b57c56eb29251a534ef5aa69d828572
Tap <visibletrap@gmail.com>
2013-05-11 12:49:47 +0800

    Small clean up

--- gol.rb
+++ gol.rb
@@ -1,15 +1,9 @@
 module Gol
   def self.next_gen(cells)
-    calculate_for_living_cells(cells)
+    next_gen_for_living_cells(cells)
   end
 
-  def self.calculate_for_living_cells(cells)
-    new_cells = Set.new
-    cells.each do |c|
-      if [2,3].include?(c.alive_neighbours_count(cells))
-        new_cells << c
-      end
-    end
-    new_cells
+  def self.next_gen_for_living_cells(cells)
+    Set.new cells.select { |c| [2,3].include?(c.alive_neighbours_count(cells)) }
   end
 end
